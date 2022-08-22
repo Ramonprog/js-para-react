3f8badaac904870baf34f96950d321b23e237cb6
ramonprog <programador.ramon@gmail.com>
2022-08-22 13:43:11 -0300

    styling boardgame

--- src/componets/objects/BoardGames/index.js
+++ src/componets/objects/BoardGames/index.js
@@ -1,8 +1,12 @@
+import '../BoardGames/style.css'
 import CardGame from "../../cardgame";
 
 const BoardGame = (amoultCard) => {
     const $htmlCardGame = CardGame();
-    const $htmlBoardGame = $htmlCardGame.repeat(amoultCard)
-    return $htmlBoardGame;
+    const $htmlContente = $htmlCardGame.repeat(amoultCard)
+    return `
+    <section class="board-game">
+        ${$htmlContente}
+    </section> `;
 };
 export default BoardGame;
